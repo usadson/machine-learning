b78b7891ff2ddd07e0e3e5bd501061087732bcc9
Tristan Gerritsen <code@thewoosh.org>
2024-09-30 15:32:21 +0200

    feat: Complete thema 3 werkcollege 1

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pip install tensorflow[and-cuda] matplotlib pandas numpy scipy ipykernel scikit-learn pyarrow pyyaml h5py
+pip install tensorflow[and-cuda] matplotlib pandas numpy scipy ipykernel scikit-learn pyarrow pyyaml h5py plotnine
 conda install -c conda-forge opencv
